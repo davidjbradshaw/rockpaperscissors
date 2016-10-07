91e90f6a1141f9646bdc86067441eb2c81d12920
David J. Bradshaw <dave@bradshaw.net>
2016-10-07 15:31:10 +0100

    Dedupe code

--- test/ref.spec.js
+++ test/ref.spec.js
@@ -4,55 +4,35 @@ import { MOVES } from '../src/js/consts';
 
 describe('Judge game', () => {
 
-	function setupPLayers() {
-		return [
-				new Player('Test Player 1'),
-				new Player('Test Player 2')
-			]
+	function testPlay(p1Move, p2Move, winner) {
+		const players = [
+			new Player('Test Player 1'),
+			new Player('Test Player 2')
+		]
+
+		players[0].move = p1Move;
+		players[1].move = p2Move;
+
+		expect( judge(players) ).to.equal(winner);
 	}
 
 	it('to be a draw', () => {
-		const players = setupPLayers();
-
-		players[0].move = 0;
-		players[1].move = 0;
-
-		expect( judge(players) ).to.equal(-1);
+		testPlay(0, 0, -1);
 	});
 
 	it(`${MOVES[1].name} to beat ${MOVES[0].name}`, () => {
-		const players = setupPLayers();
-
-		players[0].move = 1;
-		players[1].move = 0;
-
-		expect( judge(players) ).to.equal(0);
+		testPlay(1, 0, 0);
 	});
 
 	it(`${MOVES[2].name} to beat ${MOVES[1].name}`, () => {
-		const players = setupPLayers();
-
-		players[0].move = 2;
-		players[1].move = 1;
-
-		expect( judge(players) ).to.equal(0);
+		testPlay(2, 1, 0);
 	});
 
 	it(`${MOVES[0].name} to beat ${MOVES[1].name}`, () => {
-		const players = setupPLayers();
-
-		players[0].move = 0;
-		players[1].move = 2;
-
-		expect( judge(players) ).to.equal(0);
+		testPlay(0, 2, 0);
 	});
 
 	it(`Player 2 to win`, () => {
-		const players = setupPLayers();
-
-		players[0].move = 2;
-		players[1].move = 0;
-
-		expect( judge(players) ).to.equal(1);
+		testPlay(2, 0, 1);
 	});
 });
