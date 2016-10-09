f213432b2d885e448690c577515375a82880433d
David J. Bradshaw <dave@bradshaw.net>
2016-10-09 15:08:40 +0100

    Move display code to own file and simplfy

--- src/js/main.js
+++ src/js/main.js
@@ -2,55 +2,29 @@ require('../css/main.scss');
 import { MOVES } from './consts';
 import Player from './player';
 import Game from './game';
-
-import { addListener, getById } from './utils';
+import { display, displayStartingImages } from './display';
+import { addListener } from './utils';
 import { ie8forEach } from './ie8';
-
-const playerImages = [
-	getById('player0Image'),
-	getById('player1Image')
-];
-
-const playerScores = [
-	getById('player0Score'),
-	getById('player1Score')
-];
-
-const resultText = getById('result');
 
 const game = new Game([
 	new Player('Player 1'),
 	new Player('Player 2')
 ]);
 
-
-function displayImage(player, idx) {
-	playerImages[idx].src = player.moveImage;
-}
-
-function displayScore(player, idx) {
-	playerScores[idx].innerHTML = player.score;
-}
-
-function display(game) {
-	game.players.forEach(
-		(player, idx) => {
-			displayImage(player, idx);
-			displayScore(player, idx);
-		}
-	);
-
-	resultText.innerHTML = game.summary;
+function bindButtons() {
+	addListener('computer', 'click', () => display( game.computer() ));
+	addListener('reset', 'click', () => display( game.reset() ));
 }
 
 
-ie8forEach();
+function init() {
+	ie8forEach();
+	displayStartingImages(game);
+	bindButtons();
+}
 
-// Display starting position
-game.players.forEach(	(player, idx) => displayImage(player, idx) ); 
+init();
 
-addListener('computer', 'click', () => display( game.computer() ));
-addListener('reset', 'click', () => display( game.reset() ));
 
 MOVES.forEach( 
 	(move, idx) => addListener(move.name.toLowerCase(), 'click', () => display( game.manual(idx) ))
