269a892ec132f3e7ed1d37164b53d9573aa88a73
David J. Bradshaw <dave@bradshaw.net>
2016-10-09 20:49:46 +0100

    simplify

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,9 @@ import Game from './game';
 import start from './ui';
 import { ie8forEach } from './ie8';
 
-const game = new Game([
-	new Player('Player 1'),
-	new Player('Player 2')
-]);
-
 ie8forEach();
 
-start(game);
+start( new Game([
+	new Player('Player 1'),
+	new Player('Player 2')
+]));
